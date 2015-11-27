03ead064742ab87097be0496a226ec66435c722d
refond <refon@pobox.com>
2015-11-27 16:20:42 +0100

    Add basic Psmod1Component trait with wave(message) implementation. Add basic Psmod1ComponentImpl class to hook up to ApplicationLifecycle.

---
+++ app/controllers/Psmod1Component.scala
@@ -0,0 +1,35 @@
+package controllers
+
+import javax.inject.Inject
+import play.api.inject.ApplicationLifecycle
+import scala.concurrent.Future
+import play.api.Logger
+
+
+/**
+ * Basic module component for proof of concept.
+ * 
+ * Logs and returns a string message unchanged.
+ */
+trait Psmod1Component {
+  def wave (message : String) : String = {
+    Logger.debug(s"Psmod1Component waving $message")
+    message
+  }
+}
+
+/**
+ * Example of a component named Psmod1ComponentImpl requiring the ApplicationLifecycle component.
+ */
+class Psmod1ComponentImpl @Inject() (lifecycle: ApplicationLifecycle) extends Psmod1Component {
+
+  // Executed at module start
+  Logger.debug("Psmod1ComponentImpl application lifecycle startup.")
+  
+  lifecycle.addStopHook { () =>
+    // Executed at module stop
+    Logger.debug("Psmod1ComponentImpl application lifecycle stop hook executed.")
+    Future.successful(())
+  }
+  
+}
---
+++ app/controllers/Psmod1Module.scala
@@ -0,0 +1,20 @@
+package controllers
+
+import play.api.Configuration
+import play.api.Environment
+import play.api.inject.Binding
+import play.api.inject.Module
+
+/**
+ * Binds components to their implementations. Configuration and Environment are accessible 
+ * but not making any use of them for simplicity sake.
+ */
+class Psmod1Module extends Module {
+  def bindings(environment: Environment, configuration: Configuration): Seq[Binding[_]] = {
+    Seq(
+      bind[Psmod1Component].to[Psmod1ComponentImpl]
+    )
+  }
+}
+  
+

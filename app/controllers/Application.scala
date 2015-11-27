1a74012f423587db03abebf7b82c025d8dacb3d4
refond <refon@pobox.com>
2015-11-27 16:33:17 +0100

    Step 1 - Defined psmod1 controllers.Psmod1Module as a play module to test module loading, application lifecycle start/stop.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,10 +3,16 @@ package controllers
 import play.api._
 import play.api.mvc._
 
-class Application extends Controller {
+import javax.inject.Inject
+
+/**
+ * Constructor injection of Psmod1Component
+ */
+class Application @Inject() (psmod1: Psmod1Component) extends Controller {
 
   def index = Action {
-    Ok(views.html.index("Message from psmod1"))
+    // Basic test of Psmod1Component component wave method call. (Not a pure function, it logs the message.)
+    Ok(views.html.index(psmod1.wave("Message from psmod1")))
   }
 
 }

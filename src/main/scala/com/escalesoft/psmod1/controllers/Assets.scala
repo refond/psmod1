47658ab7707422ac263baf930b6d26db7701cd4e
refond <refon@pobox.com>
2015-12-03 22:49:23 +0100

    Implement module specific Assets class.

--- src/main/scala/com/escalesoft/psmod1/controllers/Assets.scala
+++ src/main/scala/com/escalesoft/psmod1/controllers/Assets.scala
@@ -1,7 +1,12 @@
 package com.escalesoft.psmod1.controllers
 
-import controllers._
 
 import play.api.http.LazyHttpErrorHandler
 
-object Assets extends AssetsBuilder(LazyHttpErrorHandler)
+/**
+ * Assets in module specific package 
+ * 
+ * This to avoid routes clashes with project using module own routes.
+ * Check sub-module doc: https://www.playframework.com/documentation/2.4.x/SBTSubProjects#Assets-and-controller-classes-should-be-all-defined-in-the-controllers.admin-package
+ */
+class Assets extends controllers.AssetsBuilder(LazyHttpErrorHandler)
--- src/main/scala/controllers/psmod1/Assets.scala
+++
@@ -1,6 +0,0 @@
-package controllers.psmod1
-
-import play.api.http.LazyHttpErrorHandler
-
-object Assets extends controllers.AssetsBuilder(LazyHttpErrorHandler)
- 
